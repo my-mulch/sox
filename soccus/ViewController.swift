2f27c2dc89d07431bf0fa8a294d7007f85416be3
tpurne984 <Truman_Purnell@comcast.com>
2019-04-19 17:17:00 -0700

    creates working view controller for raw camera data

--- soccus/ViewController.swift
+++ soccus/ViewController.swift
@@ -1,20 +1,89 @@
 //
 //  ViewController.swift
-//  soccus
+//  RectCapture
 //
-//  Created by Truman Purnell on 4/19/19.
-//  Copyright © 2019 Truman Purnell. All rights reserved.
+//  Created by Ben Scheirman on 6/27/17.
+//  Copyright © 2017 NSScreencast. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
+import CoreImage
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    lazy var captureSession: AVCaptureSession = {
+        let session = AVCaptureSession()
+        session.sessionPreset = .high
+        return session
+    }()
+    
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    let sampleBufferQueue = DispatchQueue.global(qos: .userInteractive)
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.setupCaptureSession()
     }
 
-
+    // MARK: - Camera Capture
+    
+    private func findCamera() -> AVCaptureDevice? {
+        let deviceTypes: [AVCaptureDevice.DeviceType] = [
+            .builtInDualCamera,
+            .builtInTelephotoCamera,
+            .builtInWideAngleCamera
+        ]
+        
+        let discovery = AVCaptureDevice.DiscoverySession(deviceTypes: deviceTypes,
+                                                         mediaType: .video,
+                                                         position: .front)
+        
+        return discovery.devices.first
+    }
+    
+    private func setupCaptureSession() {
+        guard captureSession.inputs.isEmpty else { return }
+        guard let camera = findCamera() else { return }
+        
+        do {
+            let cameraInput = try AVCaptureDeviceInput(device: camera)
+            captureSession.addInput(cameraInput)
+            
+            let preview = AVCaptureVideoPreviewLayer(session: captureSession)
+            preview.frame = view.bounds
+            preview.backgroundColor = UIColor.black.cgColor
+            preview.videoGravity = .resizeAspect
+            view.layer.addSublayer(preview)
+            self.previewLayer = preview
+            
+            let output = AVCaptureVideoDataOutput()
+            output.alwaysDiscardsLateVideoFrames = true
+            output.setSampleBufferDelegate(self, queue: sampleBufferQueue)
+            
+            captureSession.addOutput(output)
+            
+            captureSession.startRunning()
+            
+        } catch let e {
+            print("Error creating capture session: \(e)")
+            return
+        }
+    }
+    
 }
 
+extension ViewController : AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        print("sampleBuffer")
+    }
+}
